0e0e8ebfd67e4b9a4375a5fea2feb1a71fdda1bd
lab304-18 <lab304-18@labeng.local>
2020-03-10 17:57:45 +0700

    Lab18 first work

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -8,10 +8,50 @@ struct Rect{
 double overlap(Rect,Rect);
 
 int main(){
+	double x1,y1,w1,h1;
+	double x2,y2,w2,h2;
 	cout << "Please input Rect 1 (x y w h): ";
+	cin >> x1 >> y1 >> w1 >> h1 ;
+	Rect R1 = {x1,y1,w1,h1};
 	cout << "Please input Rect 2 (x y w h): ";
-	
-	
-	cout << "Overlap area = ";	
+	cin >> x2 >> y2 >> w2 >> h2 ;
+	Rect R2 = {x2,y2,w2,h2} ;
+	cout << "Overlap area = ";
+	cout << overlap(R1,R2);
 	return 0;
+}
+
+double overlap(Rect R1,Rect R2){
+	double Wmax,Wmin,Hmax,Hmin,Woverlap,Hoverlap;
+	if(R1.x < R2.x){
+		Wmax = R2.x;
+	}else{
+		Wmax = R1.x;
+	}
+	if(R1.x+R1.w < R2.x+R2.w){
+		Wmin = R1.x+R1.w;
+	}else{
+		Wmin = R2.x+R2.w;
+	}
+	if(R1.y-R1.h < R2.y-R2.h){
+		Hmax = R2.y-R2.h;
+	}else{
+		Hmax = R1.y-R1.h;
+	}
+	if(R1.y < R2.y){
+		Hmin = R1.y;
+	}else{
+		Hmin = R2.y;
+	}
+	if(Wmin > Wmax){
+		Woverlap = Wmin - Wmax;
+	}else{
+		Woverlap = 0;
+	}
+	if(Hmin > Hmax){
+		Hoverlap = Hmin - Hmax;
+	}else{
+		Hoverlap = 0;
+	}
+	return Hoverlap*Woverlap;
 }
